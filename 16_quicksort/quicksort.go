00fbb894ae6817059366ed041cd59da425d31cf8
riddledwithknife <riddledwithknife@icloud.com>
2024-05-06 18:45:59 +0300

    [upd] Fixed BS and QS tasks

--- 16_quicksort/quicksort.go
+++ 16_quicksort/quicksort.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
+
+func quickSort(arr []int, s, e int) []int { // Принимаем слайс, начальный индекс и количество элементов
+	if e-s+1 <= 1 { // Если количство элементов равно 1 или нулю, то возвращаем слайс
+		return arr
+	}
+
+	pivot := arr[e] // Опорный элемент (Последний)
+	left := s       // Левый указатель
+
+	for i := s; i < e; i++ {
+		if arr[i] < pivot {
+			arr[i], arr[left] = arr[left], arr[i] // Обмен переменной
+			left++                                // Сдвиг левого указателя
+		}
+	}
+
+	arr[e] = arr[left]
+	arr[left] = pivot
+
+	quickSort(arr, s, left-1) // Рекурсивный вызов левой части
+	quickSort(arr, left+1, e) // Рекурсивный вызов правой части
+
+	return arr
+}
 
 func main() {
 	slice := []int{5, 4, 3, 6, 8, 10, 20, 17, 6}
-	slices.Sort(slice) // Сортиурем слайс
+
+	slice = quickSort(slice, 0, len(slice)-1)
+
 	fmt.Println(slice) // Печатаем
 }
--- 17_binarysearch/binarysearch.go
+++ 17_binarysearch/binarysearch.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
+
+func binarySearch(arr []int, target int) int {
+	var L int         // Левый указатель
+	R := len(arr) - 1 // Правый указатель
+
+	for L <= R {
+		mid := (L + R) / 2 // Центр
+
+		if target > arr[mid] { // Если искомое значение больше значения по середине, то сдвигаем левый указатель
+			L = mid + 1
+		} else if target < arr[mid] { // Если искомое значение меньше значения по середине, то сдвигаем правый указатель
+			R = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1 // Если такого значения нет, то возвращаем -1
+}
 
 func main() {
 	slice := []int{1, 2, 3, 4, 5}
-	pos, in := slices.BinarySearch(slice, 5) // Бинарный поиск
-	fmt.Println(slice, "\n", pos, "\n", in)  // Печатаем слайс, позицию таргета, и есть ли вообще этот элемент в слайсе
+
+	pos := binarySearch(slice, 4)
+
+	fmt.Println(pos)
 }
